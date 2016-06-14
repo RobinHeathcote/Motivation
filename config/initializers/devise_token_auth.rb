3acc0d0952359d3c304390524838d2633856c6a2
letianw <tmac4ever0824@gmail.com>
2016-06-14 21:27:54 +1000

    user tests finished

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -34,4 +34,7 @@ DeviseTokenAuth.setup do |config|
   # If, however, you wish to integrate with legacy Devise authentication, you can
   # do so by enabling this flag. NOTE: This feature is highly experimental!
   # enable_standard_devise_support = false
+  # Devise.setup do |config|
+  #   config.email_regexp = /\A[^@\s]+@([^@\s]+\.)+[^@\W]+\z/
+  # end
 end
